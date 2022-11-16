0b72812c920b988ab248593b508582d8cfb97eee
shube <shubelkom@gmail.com>
2022-11-16 21:02:14 +0300

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,69 +4,6 @@ public class Main {
     private static final double MIN_RANGE = -5;
     private static final double MAX_RANGE = 5;
     private static final double EPS = 0.0001;
-    private double sqr;
-    private double lamda;
-
-    /**
-     * Метод находит начальное значение для х.
-     * Как он работает? Мы подставляем в производную формулы
-     * minRange, maxRange и x и выбираем fmax. (максимальный результат)
-     *
-     * @param minRange - минимальный корень для значения х;
-     * @param maxRange - максимальный корень для значения х;
-     * @param x        - корень, найденный из уравнения;
-     * @return начальное значение для х для итерации
-     */
-    public static double searchX(double minRange, double maxRange, double x) {
-        double result;
-        double a = getDerivative(minRange);
-        double b = getDerivative(maxRange);
-        double c = getDerivative(x);
-        if (a > b && a > c) {
-            result = minRange;
-        } else {
-            if (b > a && b > c) {
-                result = maxRange;
-            } else {
-                result = x;
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Т.к. конечная формула для итерации равна x - lambda * f(x),
-     * то этим методом мы находим значение лямбды
-     *
-     * @param x - начальное найденное значение для итераций
-     * @return лямбду
-     */
-    public static double getLambda(double x) {
-        return 1. / (getDerivative(x));
-    }
-
-    /**
-     * Ищем итерацией решение функции с погрешностью.
-     *
-     * @param lambda - лямбда;
-     * @param x      - начальное значение х для итераций;
-     * @param eps    - погрешность;
-     * @return возвращаем решение
-     */
-    private static double function(double lambda, double x, double eps) {
-        double x0;
-        double fx;
-        int count = 0;
-        System.out.printf("%s %8s %15s\n", "Iterations", "x", "f(x)");
-        do {
-            x0 = x;
-            x = x - lambda * getValue(x);
-            fx = getValue(x);
-            System.out.printf("%5d %15.8f %15.8f\n", ++count, x0, fx);
-        } while (Math.abs(x - x0) > eps);
-        return x0;
-    }
-
 
     public static void main(String[] args) {
 
@@ -75,20 +12,35 @@ public class Main {
         System.out.printf("Segment: [%.2f, %.2f];\n", MIN_RANGE, MAX_RANGE);
         System.out.printf(" Intermediate root: %.2f\n\n", x);
 
-        x = searchX(MIN_RANGE, MAX_RANGE, x);
-        double lambda = getLambda(x);
+        double result;
+        double a = Math.exp(MIN_RANGE) - 10;
+        double b = Math.exp(MAX_RANGE) - 10;
+        double c = Math.exp(x) - 10;
+        if (a > b && a > c) {
+            x = MIN_RANGE;
+        } else {
+            if (b > a && b > c) {
+                x = MAX_RANGE;
+            }
+        }
+
+        double lambda = 1. / (Math.exp(x) - 10);
         System.out.printf("Initial x for iterations: %.2f;\n", x);
         System.out.printf("Lambda: %.5f;\n\n", lambda);
 
-        double result = function(lambda, x, EPS);
+        double x0;
+        double fx;
+        int count = 0;
+        System.out.printf("%s %8s %15s\n", "Iterations", "x", "f(x)");
+        do {
+            x0 = x;
+            x = x - lambda * (Math.exp(x) - 10 * x);
+            fx = Math.exp(x) - 10 * x;
+            System.out.printf("%5d %15.8f %15.8f\n", ++count, x0, fx);
+        } while (Math.abs(x - x0) > EPS);
+
+        result = x0;
         System.out.printf("\nAnswer:\n   x = %.8f;", result);
     }
 
-    public static double getValue(double x) {
-        return Math.exp(x) - 10 * x;
-    }
-
-    public static double getDerivative(double x) {
-        return Math.exp(x) - 10;
-    }
 }
